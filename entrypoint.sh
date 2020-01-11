61dd4db0800b5515baedc93fe4139c4e4fd52872
Niki Iva <do20150208@yahoo.com>
2020-01-12 00:35:02 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+APIKEY=${APIKEY-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)}
+INSTANCEID=${INSTANCEID-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 100 | head -n 1)}
+if [ ! -e /config/ServerConfig.json ]; then
+	sed 's/<apikey>/'${APIKEY}'/;s/<instanceid>/'${INSTANCEID}'/' /tmp/ServerConfig.json > /config/ServerConfig.json
+fi
+
 set -eo pipefail
 
 # ANSI colour escape sequences
